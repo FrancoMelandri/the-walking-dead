b1d21b4539e03f5fa60e22729c69e3813dd23fff
Franco Melandri <franco.melandri@gmail.com>
2023-07-25 05:40:04 +0200

    intoducing step repository

---
+++ src/WalkingDead/Models/StepEntity.cs
@@ -0,0 +1,9 @@
+
+namespace WalkingDead;
+
+public class StepEntity
+{
+    public string Context { get; set; }
+    public string Step { get; set; }
+    public string Output { get; set; }
+}
---
+++ src/WalkingDead/Services/StepRepository.cs
@@ -0,0 +1,15 @@
+
+using TinyFp;
+
+namespace WalkingDead;
+
+public interface IStepRepository
+{
+    Either<string, StepEntity> Upsert(StepEntity entity);
+}
+
+public class StepRepository : IStepRepository
+{
+    public Either<string, StepEntity> Upsert(StepEntity entity)
+        =>  Either<string, StepEntity>.Right(entity);
+}
